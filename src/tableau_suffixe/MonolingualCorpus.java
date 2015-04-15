012fbe054bd56ccbd710dea099555adf62e895ea
jpreisner <jpreisner@free.fr>
2015-04-15 11:38:21 +0200

    Ajout de la classe SuffixArray

---
+++ src/tableau_suffixe/MonolingualCorpus.java
@@ -0,0 +1,26 @@
+package tableau_suffixe;
+
+public class MonolingualCorpus {
+	/**
+	 * Attributs (à compléter)
+	 */
+	
+	
+	
+	/**
+	 * Constructeur (à compléter)
+	 */
+	public MonolingualCorpus(){
+		
+	}
+
+	/** Permet de charger le corpus, le segmenter en tokens 
+	 *  et créer les structures de données associées
+	 * @param fileName
+	 * @return true si le chargement se fait correctement, false sinon
+	 */
+	public boolean loadFromFile(String fileName){
+		return false;
+	}
+	
+}
