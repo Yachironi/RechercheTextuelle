161f8bc726395ace694d02e65fcd204009071f26
Yasser RABI <yachironi@hotmail.com>
2015-04-15 11:52:06 +0200

    add principal methodes

---
+++ src/utility/Corpus.java
@@ -0,0 +1,19 @@
+package utility;
+
+public class Corpus {
+	public boolean loadFromFile(String fileName) {
+		return true;
+	}
+
+	String getTokenAtPosition(int position) {
+		return null;
+	}
+
+	String getSuffixFromPosition(int position) {
+		return null;
+	}
+
+	int compareSuffixes(int position1, int position2) {
+		return 0;
+	}
+}
