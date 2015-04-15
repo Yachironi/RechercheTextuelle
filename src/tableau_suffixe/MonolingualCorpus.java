f2a1bb343addbb3ff28af130cf77023014f33354
jpreisner <jpreisner@free.fr>
2015-04-15 11:54:29 +0200

    ajout méthodes

--- src/tableau_suffixe/MonolingualCorpus.java
+++ src/tableau_suffixe/MonolingualCorpus.java
@@ -23,4 +23,16 @@ public class MonolingualCorpus {
 		return false;
 	}
 	
+	public String getTokenAtPosition(int position){
+		return null;
+	}
+	
+	public String getSuffixFromPosition(int position){
+		return null;
+	}
+	
+	public int compareSuffixes(int position1, int position2){
+		return 0;
+	}
+	
 }
